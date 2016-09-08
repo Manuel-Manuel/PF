734427ce51c78390989534569fb9b8f5e7729a46
Manuel-Manuel <cerverus753@gmail.com>
2016-09-08 16:38:21 +0200

    Create red8.js

---
+++ red8.js
@@ -0,0 +1,3 @@
+if (window.innerWidth>700){console.log('meh')}else{function obi23() {var kol909=window.location.pathname,l989=0,lop00=['/p/disattiva-il-tuo-anti-block.html','/p/disattiva-il-tuo-anti-block.html?m=1'];for(var iii=0,il08=lop00.length; iii<il08;iii++){if (kol909===lop00[iii]){console.log('meh')}else{l989++}}
+if(l989>=2){window.location.assign("https://psicologiafemminile.blogspot.com/p/disattiva-il-tuo-anti-block.html")}}
+var pp998=document.getElementById('AdSense1');if (pp998=== null||pp998.style.display==='none'||pp998.style.visibility==='hidden'||pp998.clientHeight===0){obi23()}}
