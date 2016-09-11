a19d3fe3e835c4819a36697949d92aeb20b03910
Manuel-Manuel <cerverus753@gmail.com>
2016-09-11 15:58:06 +0200

    Create MM3rd.js

---
+++ MM3rd.js
@@ -0,0 +1,2 @@
+if (window.innerWidth<700){function obi23() {var kol909=window.location.pathname,l989=0,lop00=['/p/disattiva-il-tuo-ad-block.html#noad','/p/disattiva-il-tuo-ad-block.html?m=1#noad'];if (kol909!==lop00[0]){l989++}if(kol909!==lop00[1]){l989++}if(l989>=2){window.location.assign("https://mondomaschile.blogspot.it/p/disattiva-il-tuo-ad-block.html?m=1#noad")}}
+var pp998=document.getElementById('AdSense1');if (pp998=== null||pp998.style.display==='none'||pp998.style.visibility==='hidden'||pp998.clientHeight===0){obi23()}}
