d828829d3a17df47c21755c2dc2656899a774622
Manuel-Manuel <cerverus753@gmail.com>
2016-09-11 21:59:37 +0200

    Delete SB.js

--- SB.js
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" rel="axis">
-if (window.innerWidth<700){function obi23() {var kol909=window.location.pathname,l989=0,lop00=['/p/disattiva-il-tuo-ad-block.html#ad','/p/disattiva-il-tuo-ad-block.html?m=1#ad'];if (kol909!==lop00[0]){l989++}if(kol909!==lop00[1]){l989++}if(l989>=2){window.location.assign("http://sono-brutta.blogspot.com/p/disattiva-il-tuo-ad-block.html?m=1#ad")}}
-var pp998=document.getElementById('AdSense1');if (pp998=== null||pp998.style.display==='none'||pp998.style.visibility==='hidden'||pp998.clientHeight===0){obi23()}}</script>
